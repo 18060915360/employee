f2c9ca5727d9e3c01d754669a0bf48af54b4b911
wbh <731576347@qq.com>
2022-09-18 12:22:34 +0800

    Revert "commit delete-service-3 modify-product-1"
    This reverts commit fb91544

---
+++ employee1/service/IPaymentInfoService.java
@@ -0,0 +1,7 @@
+package com.atguigu.paymentdemo.service;
+
+import com.atguigu.paymentdemo.entity.PaymentInfo;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+public interface IPaymentInfoService extends IService<PaymentInfo>{
+}
---
+++ employee1/service/impl/RefundInfoServiceImpl.java
@@ -0,0 +1,11 @@
+package com.atguigu.paymentdemo.service.impl;
+
+import com.atguigu.paymentdemo.entity.RefundInfo;
+import com.atguigu.paymentdemo.mapper.RefundInfoMapper;
+import com.atguigu.paymentdemo.service.IRefundInfoService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RefundInfoServiceImpl extends ServiceImpl<RefundInfoMapper,RefundInfo> implements IRefundInfoService {
+}
