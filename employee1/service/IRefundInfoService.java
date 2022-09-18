fb91544423756a1d92016179435f921bf33a0002
wbh <731576347@qq.com>
2022-09-18 12:19:32 +0800

    commit delete-service-3 modify-product-1

--- employee1/service/IRefundInfoService.java
+++
@@ -1,7 +0,0 @@
-package com.atguigu.paymentdemo.service;
-
-import com.atguigu.paymentdemo.entity.RefundInfo;
-import com.baomidou.mybatisplus.extension.service.IService;
-
-public interface IRefundInfoService extends IService<RefundInfo>{
-}
