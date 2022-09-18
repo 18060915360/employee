2dc8599cdcc5e0ec45fcb90ac45f7cbcbf4720eb
wbh <731576347@qq.com>
2022-09-18 12:58:36 +0800

    commit delete service -3

--- employee1/service/IPaymentInfoService.java
+++
@@ -1,7 +0,0 @@
-package com.atguigu.paymentdemo.service;
-
-import com.atguigu.paymentdemo.entity.PaymentInfo;
-import com.baomidou.mybatisplus.extension.service.IService;
-
-public interface IPaymentInfoService extends IService<PaymentInfo>{
-}
