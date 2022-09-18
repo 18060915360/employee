3b1c19555907d2cf42df8e2fdb101e374b6b3709
wbh <731576347@qq.com>
2022-09-18 12:51:13 +0800

    Revert 6804164ad9cab124a6884be6fb3511efeb36f1cd

--- employee1/service/IProductService.java
+++
@@ -1,8 +0,0 @@
-package com.atguigu.paymentdemo.service;
-
-import com.atguigu.paymentdemo.entity.Product;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.baomidou.mybatisplus.extension.service.IService;
-
-public interface IProductService extends IService<Product>{
-}
